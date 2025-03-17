77c455cc8c050424c5112ca48b4490b454f84f07
Noriks29 <sehinr292@gmail.com>
2025-03-17 22:18:22 +0300

    немного изменённые вкладки кака-лидеры

--- frontend/src/components/PagesTab/LeaderConstellationConstellation.vue
+++ frontend/src/components/PagesTab/LeaderConstellationConstellation.vue
@@ -1,41 +1,51 @@
 <template>
-    <div class="main_contain">
+    <div class="main_contain RowSection">
           <DefaultTable v-if="ShowDefaultTable" :dataLableName="dataLableName" :dataTable="dataTable" @closetable="ShowDefaultTable = false"/>
           <div>
             <button class="ToMenuButtonDiv" @click="SelectComponent('TemplateComponent')">
               <img src="../../assets/exit.svg">
             </button>
+            <div>План контактов КА – КА</div>
           </div>
-          
-          
     <div class="ContentDiv">
-      <div class="TitleText">План контактов КА – КА</div>
-      <div class="Panel">
+      <div class="Panel LeftPanel">
           <div>Парамертры системы</div>
             <div class="SystemInfo">
               <table>
-                  <tr><td>Начальное время расчетов:</td>
-                      <td v-html="CreateDateTime(systemStatus.startTime)"></td>
-                    </tr>
-                    <tr><td>Начало горизонта моделирования:</td>
-                      <td v-html="CreateDateTime(systemStatus.modelingBegin)"></td>
-                    </tr>
-                    <tr><td>Окончание горизонта моделирования:</td>
-                      <td v-html="CreateDateTime(systemStatus.modelingEnd)"></td>
-                    </tr>
-                  <tr><td>Шаг моделирования:</td><td>{{ systemStatus.step }}</td></tr>
-                  <tr><td>Минимальный угол</td><td><input type="number" :value="experimentObject.angle" @change="experimentObject.angle=Number($event.target.value)"></td></tr>
+                  <tr><th>Начальное время расчетов:</th></tr>
+                      <tr><td v-html="CreateDateTime(systemStatus.startTime)"></td></tr>
+                  <tr><th>Начало горизонта моделирования:</th></tr>
+                      <tr><td v-html="CreateDateTime(systemStatus.modelingBegin)"></td></tr>
+                  <tr><th>Окончание горизонта моделирования:</th></tr>
+                      <tr><td v-html="CreateDateTime(systemStatus.modelingEnd)"></td></tr>
+                  <tr><th>Шаг моделирования: {{ systemStatus.step }}</th></tr>
+                  <tr><th><div>Минимальный угол <input type="number" :value="experimentObject.angle" @change="experimentObject.angle=Number($event.target.value)"></div></th></tr>
                 </table>
             </div>
+            <div class="FlexColumn">
+              <div><button @click="CommandWork(3)" class="ButtonCommand">Топология сети</button></div>
+              <div><button @click="CommandWork(1)" class="ButtonCommand">Рассчитать окна видимости</button></div>
+              <div><button @click="CommandWork(2)" class="ButtonCommand">Показать окна видимости / плана контактов</button></div>
+              <div><button @click="CommandWork(6)" class="ButtonCommand">Графическое представление плана контактов</button></div>
+            </div>
         </div>
-    <div class="Panel MaxWidth">
-      <div class="FlexColumn">
-          <div><button @click="CommandWork(1)" class="ButtonCommand">Рассчитать окна видимости</button></div>
-          <div><button @click="CommandWork(2)" class="ButtonCommand">Показать окна видимости / плана контактов</button></div>
-          <div><button @click="CommandWork(6)" class="ButtonCommand">Графическое представление плана контактов</button></div>
+        <div class="Panel RightPanel">
+          <div>
+            <table>
+              <tr><th>Кластер</th><th>Кластер</th><th></th></tr>
+              <tr v-for="data, index in clusterTopology" :key="index">
+                <td><SelectDiv  :dataOption="lessConstellation" :valueS="{lable: data.cluster1.constellationName}" :id="index" @valueSelect="ChangeCluster($event, 'cluster1')"/></td>
+                <td><SelectDiv  :dataOption="lessConstellation" :valueS="{lable: data.cluster2.constellationName}" :id="index" @valueSelect="ChangeCluster($event, 'cluster2')"/></td>
+                <td @click="DeleteRow(index)" style="width: 20px;"><img class="iconDelete" src="../../assets/delete.svg" alt="Удалить"></td>
+              </tr>
+              <tr><td colspan="3" @click="AddRow('clusterTopology')" style="text-align: center;"><img src="../../assets/add.png" alt="" class="addButtonIcon"> Добавить</td></tr>
+            </table>
+          </div>
         </div>
-      </div>
     </div>
+
+    
+    <!--
     <div id="PlotlyDiv" v-if="ShowPlotlyContain">
       <div class="ContainerDiv">
         <div class="closebutton"><button @click="ShowPlotlyContain = false">
@@ -44,7 +54,7 @@
 
       <div id="plotlymapContain1" style="height: 79vh;"></div>
       </div>
-    </div>
+    </div>-->
     </div>
   </template>
   
@@ -52,6 +62,7 @@
 
 import {DisplayLoad, FetchGet, FetchPost} from '../../js/LoadDisplayMetod.js'
 import { PagesSettings } from './PagesSettings.js';
+import SelectDiv from '../SelectDiv.vue';
 import DefaultTable from '../DefaultTable.vue';
 import Plotly from 'plotly.js-dist'
 
@@ -59,11 +70,14 @@ import Plotly from 'plotly.js-dist'
     name: 'LeaderConstellationConstellation',
     mixins: [PagesSettings],
     components:{
-      DefaultTable
+      DefaultTable,
+      SelectDiv
     },
     data(){
       return{
         ConstellationJson: [],
+        clusterTopology: [], // топология сети
+        lessConstellation: [], // облегчённый список ог для селектора
         earthSize: 0,
         ShowDefaultTable: false,
         ShowPlotlyContain: false,
@@ -77,6 +91,34 @@ import Plotly from 'plotly.js-dist'
       }
     },
     methods: {
+      async AddRow(mode){
+        switch (mode) {
+          case 'clusterTopology':
+            this.clusterTopology.push({
+              "cluster1": this.lessConstellation[0].value,
+              "cluster2": this.lessConstellation[1].value,
+              "beginTime": null,
+              "endTime": null,
+              "deleted": null
+            })
+            await FetchPost('/api/v1/cluster/update', this.clusterTopology, null)
+            this.ReFetch()
+            break;
+        
+          default:
+            break;
+        }
+      },
+      async DeleteRow(index){
+        this.clusterTopology[index].deleted= true
+        await FetchPost('/api/v1/cluster/update', this.clusterTopology, null)
+        this.ReFetch()
+      },
+      async ChangeCluster(event, param){
+        this.clusterTopology[event.id][param] = event.value
+        await FetchPost('/api/v1/cluster/update', this.clusterTopology, null)
+        this.ReFetch()
+      },
        async CommandWork(commandId){
             console.log(commandId)
             if(commandId == 2){
@@ -187,8 +229,20 @@ import Plotly from 'plotly.js-dist'
               )
             }
         },
+        async ReFetch(){
+          this.clusterTopology = await FetchGet("/api/v1/cluster/all") || []
+        }
     },
     async mounted() {
+      this.clusterTopology = await FetchGet("/api/v1/cluster/all")
+      let rezult = await FetchGet("/api/v1/constellation/cl/all") || []
+      this.lessConstellation = []
+      rezult.forEach(element => {
+        this.lessConstellation.push({lable: element.constellationName, value: element})
+      })
+
+      
+      console.log(this.lessConstellation)
     }
   }
   </script>
@@ -199,6 +253,24 @@ import Plotly from 'plotly.js-dist'
   flex-direction: column;
   flex-wrap: nowrap;
   color: white;
+
+  &.RowSection{
+    display: flex;
+
+    .ContentDiv{
+      flex: 1;
+      display: flex;
+      flex-direction: row;
+      align-items: stretch;
+
+      .Panel{
+        &.RightPanel{
+          flex: 1;
+          overflow-y: auto;
+        }
+      }
+    }
+  }
 }
 td{
   text-align: left;
