7cd2c7c622ac52c1ddebf99f3cc8cbfbbfe18e83
Noriks29 <sehinr292@gmail.com>
2025-07-25 14:34:19 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),nodePolyfills({
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+        crypto: true
+      }
+    })],
+  optimizeDeps: {
+    exclude: ['crypto']
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
